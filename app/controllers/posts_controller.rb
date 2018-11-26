110bca897383f73b77f214ed98052587e71b29d5
leannemcabey <leanne.m.cabey@gmail.com>
2018-11-26 17:52:33 -0500

    complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,15 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    @post = set_post!
+    @test_post = Post.new(post_params)
 
-    redirect_to post_path(@post)
+    if @test_post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
